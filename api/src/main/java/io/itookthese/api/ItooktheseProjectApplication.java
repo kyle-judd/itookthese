b7c11f9770263685b86ee25dc260fdaf76d41d63
Kyle Judd <kylejudd@Kyles-MacBook-Pro.local>
2026-03-04 19:09:51 -0500

    initial project scaffold

---
+++ api/src/main/java/io/itookthese/api/ItooktheseProjectApplication.java
@@ -0,0 +1,13 @@
+package io.itookthese.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItooktheseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItooktheseProjectApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/io/itookthese/api/ItooktheseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.itookthese.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItooktheseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
