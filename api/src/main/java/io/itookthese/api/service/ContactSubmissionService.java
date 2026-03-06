7dbffc9ec286d7a6a17b7ba5b4a2d04e7bf5061c
kyle-judd <juddkyle1408@gmail.com>
2026-03-06 16:33:56 -0500

    fixed import in contact submission service

--- api/src/main/java/io/itookthese/api/service/ContactSubmissionService.java
+++ api/src/main/java/io/itookthese/api/service/ContactSubmissionService.java
@@ -6,6 +6,7 @@ import io.itookthese.api.entity.ContactSubmission;
 import io.itookthese.api.repository.ContactSubmissionRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
